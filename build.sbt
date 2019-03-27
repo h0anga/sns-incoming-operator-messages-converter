dafd6d2520107b8852538a6434c0159e5ac59276
olga.bobrovnytska <olga.bobrovnytska@bskyb.com>
2019-03-27 14:30:42 +0000

    Added zipkin tracing

--- build.sbt
+++ build.sbt
@@ -4,14 +4,19 @@ version := "0.1.1"
 
 scalaVersion := "2.12.8"
 
+resolvers += "mvnrepository" at "http://central.maven.org/maven2/"
+
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "3.0.5" % "test",
   "net.liftweb" %% "lift-json" % "3.3.0",
   "org.apache.kafka" %% "kafka-streams-scala" % "2.1.0",
   "org.apache.kafka" % "kafka-streams-test-utils" % "2.1.0" % Test,
-  "com.typesafe.scala-logging" %% "scala-logging"  % "3.5.0",
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0",
   "org.json4s" %% "json4s-native" % "3.6.3",
-  "org.json4s" %% "json4s-xml" % "3.6.5"
+  "org.json4s" %% "json4s-xml" % "3.6.5",
+  "io.zipkin.brave" % "brave-instrumentation-kafka-clients" % "5.6.3",
+  "io.zipkin.brave" % "brave-instrumentation-kafka-streams" % "5.6.3",
+  "io.zipkin.reporter2" % "zipkin-sender-kafka11" % "2.8.1"
 
 )
 
@@ -23,3 +28,5 @@ dockerBaseImage := "openjdk:8-jre"
 //dockerBaseImage := "openjdk:8-jre-alpine"
 
 mainClass in Compile := Some("converter.Xml2JsonConverterApp")
+//http://central.maven.org/maven2/io/zipkin/reporter2/zipkin-sender-kafka11/2.8.1/zipkin-sender-kafka11-2.8.1.jar
+//http://central.maven.org/maven2/io/zipkin/reporter2/zipkin-sender-kafka11_2.12/2.8.1/zipkin-sender-kafka11_2.12-2.8.1.pom
--- src/main/scala/converter/KafkaSetup.scala
+++ src/main/scala/converter/KafkaSetup.scala
@@ -2,11 +2,16 @@ package converter
 
 import java.util.Properties
 
-import converter.Xml2JsonConverter.{fromXml, xmlToJson}
+import brave.Tracing
+import brave.kafka.streams.KafkaStreamsTracing
+import brave.sampler.Sampler
+import converter.Xml2JsonConverter.xmlToJson
 import org.apache.kafka.common.serialization.{Serde, Serdes}
 import org.apache.kafka.streams.errors.LogAndContinueExceptionHandler
 import org.apache.kafka.streams.kstream.{Consumed, KStream, Predicate}
 import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig, Topology}
+import zipkin2.reporter.AsyncReporter
+import zipkin2.reporter.kafka11.KafkaSender
 
 
 class KafkaSetup(private val server: String, private val port: String) {
@@ -15,13 +20,16 @@ class KafkaSetup(private val server: String, private val port: String) {
 
   private var stream: KafkaStreams = _
 
-  val xmlPredicate: Predicate[_ >: String, _ >: String] = (_: String, value: String) => {
+  private val bootstrapServers = server + ":" + port
+
+  private val xmlPredicate: Predicate[_ >: String, _ >: String] = (_: String, value: String) => {
     value.startsWith("""<?xml version="1.0" encoding="UTF-8"?>""")
   }
 
+  private val tracing = setupTracing
+
   def start(inputTopicName: String, outputTopicName: String) = {
 
-    val bootstrapServers = server + ":" + port
 
     val streamingConfig = {
       val settings = new Properties
@@ -32,9 +40,17 @@ class KafkaSetup(private val server: String, private val port: String) {
       settings.put(StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG, classOf[LogAndContinueExceptionHandler])
       settings
     }
+
     val topology = build(inputTopicName, outputTopicName)
-    stream = new KafkaStreams(topology, streamingConfig)
+    stream = tracing.kafkaStreams(topology, streamingConfig)
     stream.start()
+  }
+
+  def setupTracing: KafkaStreamsTracing = {
+    val sender = KafkaSender.newBuilder.bootstrapServers(bootstrapServers).build
+    val reporter = AsyncReporter.builder(sender).build
+    val tracing = Tracing.newBuilder.localServiceName("xml-to-json-stream-transform").sampler(Sampler.ALWAYS_SAMPLE).spanReporter(reporter).build
+    KafkaStreamsTracing.create(tracing)
   }
 
   def shutDown(): Unit = {
@@ -43,12 +59,12 @@ class KafkaSetup(private val server: String, private val port: String) {
 
   def build(inputTopicName: String, outputTopicName: String): Topology = {
     val builder = new StreamsBuilder
-
     val emptyStringPredicate: Predicate[_ >: String, _ >: String] = (_: String, value: String) => {
       value.isEmpty
     }
 
     val inputStream: KStream[String, String] = builder.stream(inputTopicName, Consumed.`with`(stringSerde, stringSerde))
+
     val xmlStream: KStream[String, String] = inputStream.filter(xmlPredicate)
     val jsonStream: KStream[String, String] = xmlStream.mapValues(line => xmlToJson(line))
     jsonStream.filterNot(emptyStringPredicate)
